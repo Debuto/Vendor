2bbcc599a9684ba3436f2f10de53d716f150dd44
Deante Cacatian <dwdeante0726@gmail.com>
2023-09-27 17:01:30 -0700

    Adding updated models.

---
+++ Vendor/Models/Orders.cs
@@ -0,0 +1,6 @@
+public class Order
+{
+    public int Id { get; set; }
+    public string Description { get; set; }
+    public int VendorId { get; set; }
+}
---
+++ Vendor/Models/Vendors.cs
@@ -0,0 +1,5 @@
+public class Vendor
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
