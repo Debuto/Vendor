5f557287a14959f2e0a26836b6d9fe9a950130e3
Debo <dwdeante0726@gmail.com>
2023-09-30 23:07:36 -0700

    Adding repo structure to test environment. Creating test for orders!

---
+++ Vendor.Tests/Model Tests/OrderTests.cs
@@ -0,0 +1,78 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vendors.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Vendors.Tests
+{
+    [TestClass]
+    public class OrderTests : IDisposable
+    {
+        public void Dispose()
+        {
+            Order.ClearAll();
+        }
+            [TestMethod]
+        public void GetOrderPrice_ReturnsOrderPrice_String()
+        {
+            string orderPrice = "Order Price";
+            Order newOrder = new Order(orderPrice, "Order Description", "Order Date");
+            string result = newOrder.OrderPrice;
+            Assert.AreEqual(orderPrice, result);
+        }
+
+            [TestMethod]
+        public void GetOrderDescription_ReturnsOrderDescription_String()
+        {
+            string orderDescription = "Order Description";
+            Order newOrder = new Order("Order Price", orderDescription, "Order Date");
+            string result = newOrder.OrderDescription;
+            Assert.AreEqual(orderDescription, result);
+        }
+
+            [TestMethod]
+        public void GetOrderDate_ReturnsOrderDate_String()
+        {
+            string orderDate = "Order Date";
+            Order newOrder = new Order("Order Price", "Order Description", orderDate);
+            string result = newOrder.OrderDate;
+            Assert.AreEqual(orderDate, result);
+        }
+
+            [TestMethod]
+        public void GetOrderId_ReturnsOrderId_Int()
+        {
+            Order.ClearAll();
+            Order newOrder = new Order("Order Price", "Order Description", "Order Date");
+            int result = newOrder.Id;
+            Assert.AreEqual(1, result);
+        }
+
+            [TestMethod]
+        public void GetAll_ReturnsAllOrderObjects_OrderList()
+        {
+            Order.ClearAll();
+            Order newOrder1 = new Order("Order Price 1", "Description 1", "Date 1");
+            Order newOrder2 = new Order("Order Price 2", "Description 2", "Date 2");
+            List<Order> expectedList = new List<Order> { newOrder1, newOrder2 };
+            List<Order> result = Order.GetAll();
+            CollectionAssert.AreEqual(expectedList, result);
+        }
+
+            [TestMethod]
+        public void Find_ReturnsCorrectOrder_Order()
+        {
+            Order.ClearAll();
+            Order newOrder1 = new Order("Order Price 1", "Description 1", "Date 1");
+            Order newOrder2 = new Order("Order Price 2", "Description 2", "Date 2");
+            Order result = Order.Find(2);
+            Assert.AreEqual(newOrder2, result);
+        }
+
+
+
+    }
+
+      
+
+}
--- Vendor/Models/Orders.cs
+++ Vendor/Models/Orders.cs
@@ -10,5 +10,30 @@ namespace Vendor.Models
     public string OrderDate {get; set;}
     public int Id {get; }
     private static List<Order> _instances = new List<Order> { };
+
+    public Order(string orderPrice, string orderDescription, string orderDate)
+    {
+      OrderPrice = orderPrice;
+      OrderDescription = orderDescription;
+      OrderDate = orderDate;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+
+        public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
   }
 }
